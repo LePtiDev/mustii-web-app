ef6339de985742ccd393f01faddb8f9b78d5aa93
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-01-29 23:58:00 +0100

    add index function on the home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    // function to get home page
+    public function index(){
+
+        return view('front.home');
+    }
 }
