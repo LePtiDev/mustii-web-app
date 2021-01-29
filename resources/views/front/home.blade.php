2a8ef71c52b7a648b434aebba0c7a6a6ff941d14
Quentin Guerrier <75quentinguerrier@gmail.com>
2021-01-30 00:04:23 +0100

    update home view with presentation

---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mustii</title>
+</head>
+<body>
+    <h1>Bienvenue</h1>
+    <h2>Liste des projets</h2>
+
+    <h3>Projet 1</h3>
+    <ul>
+        <li>Nom du projet: </li>
+        <li>Description du projet: </li>
+        <li>Image du projet : </li>
+        <li>lien du projet : </li>
+    </ul>
+
+
+    <h3>Projet 2</h3>
+    <ul>
+        <li>Nom du projet: </li>
+        <li>Description du projet: </li>
+        <li>Image du projet : </li>
+        <li>lien du projet : </li>
+    </ul>
+
+
+    <h3>Projet 3</h3>
+    <ul>
+        <li>Nom du projet: </li>
+        <li>Description du projet: </li>
+        <li>Image du projet : </li>
+        <li>lien du projet : </li>
+    </ul>
+
+
+    <h2>Formulaire</h2>
+</body>
+</html>
